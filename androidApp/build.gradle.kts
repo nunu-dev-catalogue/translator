6607265f31a64ce9c788953fb73a58b3ebad468d
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-10-18 18:36:06 +0900

    Apply bundles

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -41,9 +41,6 @@ android {
 
 dependencies {
     implementation(projects.shared)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.tooling.preview)
-    implementation(libs.compose.material3)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.compose)
     debugImplementation(libs.compose.ui.tooling)
 }
