b70ec5191c1c4d0cf10123fc372e0b3f818597a0
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-10-25 10:33:32 +0900

    Add coil image loader dependency

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
     }
     buildFeatures {
         compose = true
@@ -35,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -52,5 +52,6 @@ dependencies {
     ksp(libs.hilt.plugin)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.android)
+    implementation(libs.coil.compose)
     debugImplementation(libs.compose.ui.tooling)
 }
