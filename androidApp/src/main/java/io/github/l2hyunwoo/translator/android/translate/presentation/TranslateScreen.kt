4990efd9611f74e6ed1fe030d87f6fb39b6d5739
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-06 15:47:28 +0900

    Add FAB in TranslateScreen

--- androidApp/src/main/java/io/github/l2hyunwoo/translator/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/io/github/l2hyunwoo/translator/android/translate/presentation/TranslateScreen.kt
@@ -10,22 +10,35 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import io.github.l2hyunwoo.translator.android.R
 import io.github.l2hyunwoo.translator.android.translate.presentation.components.LanguageDropDown
 import io.github.l2hyunwoo.translator.android.translate.presentation.components.SwapLanguagesButton
+import io.github.l2hyunwoo.translator.android.translate.presentation.components.TranslateHistoryItem
 import io.github.l2hyunwoo.translator.android.translate.presentation.components.TranslateTextField
 import io.github.l2hyunwoo.translator.android.translate.presentation.components.rememberTextToSpeech
+import io.github.l2hyunwoo.translator.translate.domain.translate.TranslateError
 import io.github.l2hyunwoo.translator.translate.presentation.TranslateEvent
 import io.github.l2hyunwoo.translator.translate.presentation.TranslateState
 import java.util.Locale
@@ -36,10 +49,36 @@ fun TranslateScreen(
     onEvent: (TranslateEvent) -> Unit
 ) {
     val context = LocalContext.current
+    LaunchedEffect(state.error) {
+        val message = when (state.error) {
+            TranslateError.SERVICE_UNAVAILABLE -> context.getString(R.string.error_service_unavailable)
+            TranslateError.CLIENT_ERROR -> context.getString(R.string.client_error)
+            TranslateError.SERVER_ERROR -> context.getString(R.string.server_error)
+            TranslateError.UNKNOWN_ERROR -> context.getString(R.string.unknown_error)
+            else -> null
+        }
+        message?.let {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+            onEvent(TranslateEvent.OnErrorSeen)
+        }
+    }
     Scaffold(
         floatingActionButton = {
-
-        }
+            FloatingActionButton(
+                onClick = {
+                    onEvent(TranslateEvent.RecordAudio)
+                },
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier.size(75.dp)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.mic),
+                    contentDescription = stringResource(id = R.string.record_audio)
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) { padding ->
         LazyColumn(
             modifier = Modifier
@@ -131,6 +170,27 @@ fun TranslateScreen(
                     onTextFieldClick = {
                         onEvent(TranslateEvent.EditTranslation)
                     }
+                )
+            }
+
+            item {
+                if (state.history.isNotEmpty()) {
+                    Text(
+                        text = stringResource(
+                            id = R.string.history
+                        ),
+                        style = MaterialTheme.typography.h2
+                    )
+                }
+            }
+
+            items(state.history) { item ->
+                TranslateHistoryItem(
+                    item = item,
+                    onClick = {
+                        onEvent(TranslateEvent.SelectHistoryItem(item))
+                    },
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
         }
