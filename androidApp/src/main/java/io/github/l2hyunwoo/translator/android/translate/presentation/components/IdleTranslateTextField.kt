ef2de433f7c98b2856de320de95e3bab93d70ff3
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-06 14:33:27 +0900

    Add Idle, Translated Text Field

---
+++ androidApp/src/main/java/io/github/l2hyunwoo/translator/android/translate/presentation/components/IdleTranslateTextField.kt
@@ -0,0 +1,62 @@
+package io.github.l2hyunwoo.translator.android.translate.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import io.github.l2hyunwoo.translator.android.R
+import io.github.l2hyunwoo.translator.android.core.theme.LightBlue
+
+@Composable
+private fun IdleTranslateTextField(
+    fromText: String,
+    isTranslating: Boolean,
+    onTextChange: (String) -> Unit,
+    onTranslateClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var isFocused by remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = fromText,
+            onValueChange = onTextChange,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+            modifier = Modifier
+                .fillMaxSize()
+                .onFocusChanged { isFocused = it.isFocused },
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        )
+        if (fromText.isEmpty() && !isFocused) {
+            Text(
+                text = stringResource(
+                    id = R.string.enter_a_text_to_translate
+                ),
+                color = LightBlue
+            )
+        }
+        ProgressButton(
+            text = stringResource(
+                id = R.string.translate
+            ),
+            isLoading = isTranslating,
+            onClick = onTranslateClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+    }
+}
---
+++ androidApp/src/main/java/io/github/l2hyunwoo/translator/android/translate/presentation/components/TranslatedTextField.kt
@@ -0,0 +1,97 @@
+package io.github.l2hyunwoo.translator.android.translate.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import io.github.l2hyunwoo.translator.android.R
+import io.github.l2hyunwoo.translator.android.core.theme.LightBlue
+import io.github.l2hyunwoo.translator.core.presentation.UiLanguage
+
+@Composable
+private fun TranslatedTextField(
+    fromText: String,
+    toText: String,
+    fromLanguage: UiLanguage,
+    toLanguage: UiLanguage,
+    onCopyClick: (String) -> Unit,
+    onCloseClick: () -> Unit,
+    onSpeakerClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        LanguageDisplay(language = fromLanguage)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = fromText,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            IconButton(
+                onClick = { onCopyClick(fromText) }
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.copy),
+                    contentDescription = stringResource(id = R.string.copy),
+                    tint = LightBlue
+                )
+            }
+            IconButton(onClick = onCloseClick) {
+                Icon(
+                    imageVector = Icons.Rounded.Close,
+                    contentDescription = stringResource(id = R.string.close),
+                    tint = LightBlue
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider()
+        Spacer(modifier = Modifier.height(16.dp))
+        LanguageDisplay(language = toLanguage)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = toText,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            IconButton(onClick = {
+                onCopyClick(toText)
+            }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.copy),
+                    contentDescription = stringResource(id = R.string.copy),
+                    tint = LightBlue
+                )
+            }
+            IconButton(onClick = onSpeakerClick) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.speaker),
+                    contentDescription = stringResource(id = R.string.play_loud),
+                    tint = LightBlue
+                )
+            }
+        }
+    }
+}
