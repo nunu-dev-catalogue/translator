3506ca61c2c48efee659d5ca96801b9c0963553e
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-06 14:57:06 +0900

    Add translate screen in TranslateScreen

--- androidApp/src/main/java/io/github/l2hyunwoo/translator/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/io/github/l2hyunwoo/translator/android/translate/presentation/TranslateScreen.kt
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package io.github.l2hyunwoo.translator.android.translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +13,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import io.github.l2hyunwoo.translator.android.R
 import io.github.l2hyunwoo.translator.android.translate.presentation.components.LanguageDropDown
 import io.github.l2hyunwoo.translator.android.translate.presentation.components.SwapLanguagesButton
+import io.github.l2hyunwoo.translator.android.translate.presentation.components.TranslateTextField
 import io.github.l2hyunwoo.translator.translate.presentation.TranslateEvent
 import io.github.l2hyunwoo.translator.translate.presentation.TranslateState
 
@@ -22,6 +32,7 @@ fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit
 ) {
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
 
@@ -34,7 +45,7 @@ fun TranslateScreen(
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            item {
+            item(key = "language_select_buttons") {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
@@ -72,6 +83,45 @@ fun TranslateScreen(
                         }
                     )
                 }
+            }
+            item(key = "translate_text_field") {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslationText(it))
+                    },
+                    onCopyClick = { text ->
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(text)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.copied_to_clipboard),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+                        // TODO TTS Feature
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    }
+                )
             }
         }
     }
